fd603216b1c7f30a977f272c578e3eb60eb33ef9
Pavel Sobolev <contact@paveloom.dev>
2025-09-30 22:19:01 +0300

    Server: add the `/v0/target/{id}/incidents/` endpoint.

---
+++ server/handlers/targets_id_incidents.go
@@ -0,0 +1,97 @@
+// Copyright 2025 Pavel Sobolev
+//
+// This file is part of the Reachard project, located at
+//
+//     https://reachard.paveloom.dev
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"reachard/database"
+	"reachard/database/clickhouse"
+)
+
+type TargetsIDIncidentsHandler struct {
+	TargetsIDHandler
+}
+
+func NewTargetsIDIncidentsHandler(db database.Database) TargetsIDIncidentsHandler {
+	return TargetsIDIncidentsHandler{TargetsIDHandler{Handler{DB: db}}}
+}
+
+func (handler TargetsIDIncidentsHandler) HandleGet(writer http.ResponseWriter, request *http.Request) {
+	ctx := request.Context()
+
+	sessionInfo, authenticated := handler.AuthenticateBySessionToken(writer, request)
+	if !authenticated {
+		return
+	}
+
+	targetID := handler.ParseTargetID(writer, request)
+	if targetID < 0 {
+		return
+	}
+
+	var since clickhouse.Timestamp
+	sinceString := request.URL.Query().Get("since")
+	if sinceString != "" {
+		var err error
+		since, err = strconv.ParseInt(sinceString, 10, 64)
+		if err != nil {
+			http.Error(writer, "couldn't parse the `since` query parameter", http.StatusBadRequest)
+			return
+		}
+	}
+
+	options := clickhouse.GetIncidentsOptions{Since: since}
+	incidents, err := handler.DB.ClickHouse.GetIncidents(ctx, sessionInfo.UserID, targetID, options)
+	if err != nil {
+		http.Error(writer, "internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	json, err := json.Marshal(incidents)
+	if err != nil {
+		http.Error(writer, "internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	writer.Header().Set("Content-Type", "application/json")
+	writer.Write(json)
+}
+
+func (handler TargetsIDIncidentsHandler) HandleOptions(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type")
+	writer.Header().Set("Access-Control-Allow-Methods", "GET")
+}
+
+func (handler TargetsIDIncidentsHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	handler.HandleCORS(writer, request)
+
+	switch request.Method {
+	case "GET":
+		handler.HandleGet(writer, request)
+	case "OPTIONS":
+		handler.HandleOptions(writer, request)
+	default:
+		http.Error(writer, "method not allowed", http.StatusMethodNotAllowed)
+	}
+}
