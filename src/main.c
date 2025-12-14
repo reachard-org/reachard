3d1d8399a60334fe89a34ef01b8ec1434d42d481
Pavel Sobolev <contact@paveloom.dev>
2025-12-14 12:41:45 +0300

    Dramatically change formatting style.

--- src/main.c
+++ src/main.c
@@ -30,188 +30,201 @@
 
 #define PORT 7272
 
-struct reachard_request
-{
-  void *cls;
-  struct MHD_Connection *connection;
-  const char *url;
-  const char *method;
-  const char *version;
-  const char *upload_data;
-  size_t *upload_data_size;
-  void **req_cls;
+struct reachard_request {
+    void *cls;
+    struct MHD_Connection *conn;
+    const char *url, *method, *version;
+    const char *upload_data;
+    size_t *upload_data_size;
+    void **req_cls;
 };
 
-typedef enum MHD_Result (*reachard_handler) (struct reachard_request *request);
+typedef enum MHD_Result (*reachard_handler)(struct reachard_request *request);
 
-struct reachard_connection_info
-{
-  reachard_handler handle;
-  struct MHD_PostProcessor *postprocessor;
+struct reachard_connection_info {
+    reachard_handler handle;
+    struct MHD_PostProcessor *postprocessor;
 };
 
 static void
-reachard_connection_info_destroy (struct reachard_connection_info *conn_info)
-{
-  if (!conn_info)
-    return;
-
-  if (conn_info->postprocessor)
-    MHD_destroy_post_processor (conn_info->postprocessor);
-
-  free (conn_info);
-  conn_info = NULL;
-}
-
-static enum MHD_Result
-reachard_respond (struct reachard_request *request, const char *content,
-                  const unsigned int status_code)
-{
-  struct MHD_Response *response
-      = MHD_create_response_from_buffer_static (strlen (content), content);
-  const enum MHD_Result result
-      = MHD_queue_response (request->connection, status_code, response);
-
-  MHD_destroy_response (response);
-
-  return result;
-}
-
-static enum MHD_Result
-reachard_handle_targets_get (struct reachard_request *request)
-{
-  return reachard_respond (request, "hello from targets GET!", MHD_HTTP_OK);
-}
-
-static enum MHD_Result
-reachard_handle_targets_post (struct reachard_request *request)
-{
-  struct reachard_connection_info *conn_info = *request->req_cls;
-
-  if (*request->upload_data_size > 0)
-    {
-      const enum MHD_Result result
-          = MHD_post_process (conn_info->postprocessor, request->upload_data,
-                              *request->upload_data_size);
-      if (result != MHD_YES)
-        return MHD_NO;
-
-      *request->upload_data_size = 0;
-
-      return MHD_YES;
+reachard_connection_info_destroy(struct reachard_connection_info *conn_info) {
+    if (!conn_info) {
+        return;
     }
-  else
-    return reachard_respond (request, "hello from targets POST!", MHD_HTTP_OK);
+
+    if (conn_info->postprocessor) {
+        MHD_destroy_post_processor(conn_info->postprocessor);
+    }
+
+    free(conn_info);
+    conn_info = NULL;
+}
+
+static enum MHD_Result
+reachard_respond(
+    struct reachard_request *request,
+    const char *content,
+    const unsigned int status_code
+) {
+    struct MHD_Response *response =
+        MHD_create_response_from_buffer_static(strlen(content), content);
+    const enum MHD_Result result =
+        MHD_queue_response(request->conn, status_code, response);
+
+    MHD_destroy_response(response);
+
+    return result;
+}
+
+static enum MHD_Result
+reachard_handle_targets_get(struct reachard_request *request) {
+    return reachard_respond(request, "hello from targets GET!", MHD_HTTP_OK);
+}
+
+static enum MHD_Result
+reachard_handle_targets_post(struct reachard_request *request) {
+    struct reachard_connection_info *conn_info = *request->req_cls;
+
+    if (*request->upload_data_size > 0) {
+        const enum MHD_Result result = MHD_post_process(
+            conn_info->postprocessor,
+            request->upload_data,
+            *request->upload_data_size
+        );
+        if (result != MHD_YES) {
+            return MHD_NO;
+        }
+
+        *request->upload_data_size = 0;
+
+        return MHD_YES;
+    }
+
+    return reachard_respond(request, "hello from targets POST!", MHD_HTTP_OK);
 }
 
 enum MHD_Result
-reachard_handle_targets_post_data_iterator (
-    void *cls, enum MHD_ValueKind kind, const char *key, const char *filename,
-    const char *content_type, const char *transfer_encoding, const char *data,
-    uint64_t off, size_t size)
-{
-  return MHD_YES;
+reachard_handle_targets_post_data_iterator(
+    void *cls,
+    enum MHD_ValueKind kind, const char *key,
+    const char *filename, const char *content_type, const char *transfer_encoding,
+    const char *data, uint64_t off, size_t size
+) {
+    return MHD_YES;
 }
 
 static enum MHD_Result
-reachard_handle_targets_first_call (struct reachard_request *request)
-{
-  struct reachard_connection_info *conn_info
-      = calloc (1, sizeof (struct reachard_connection_info));
-  if (!conn_info)
-    return MHD_NO;
+reachard_handle_targets_first_call(struct reachard_request *request) {
+    struct reachard_connection_info *conn_info =
+        calloc(1, sizeof(struct reachard_connection_info));
+    if (!conn_info) {
+        return MHD_NO;
+    }
 
-  if (strcmp (request->method, "GET") == 0)
-    conn_info->handle = &reachard_handle_targets_get;
-  if (strcmp (request->method, "POST") == 0)
-    {
-      conn_info->handle = &reachard_handle_targets_post;
+    if (strcmp(request->method, "GET") == 0) {
+        conn_info->handle = &reachard_handle_targets_get;
+    }
+    if (strcmp(request->method, "POST") == 0) {
+        conn_info->handle = &reachard_handle_targets_post;
 
-      conn_info->postprocessor = MHD_create_post_processor (
-          request->connection, 512,
-          &reachard_handle_targets_post_data_iterator, NULL);
+        conn_info->postprocessor = MHD_create_post_processor(
+            request->conn,
+            512,
+            &reachard_handle_targets_post_data_iterator,
+            NULL
+        );
 
-      if (!conn_info->postprocessor)
-        {
-          free (conn_info);
-          return MHD_NO;
+        if (!conn_info->postprocessor) {
+            free(conn_info);
+            return MHD_NO;
         }
     }
 
-  if (!conn_info->handle)
-    {
-      free (conn_info);
-      return reachard_respond (request, "method not allowed",
-                               MHD_HTTP_BAD_REQUEST);
+    if (!conn_info->handle) {
+        free(conn_info);
+        return reachard_respond(
+            request, "method not allowed", MHD_HTTP_BAD_REQUEST
+        );
     }
 
-  *request->req_cls = conn_info;
+    *request->req_cls = conn_info;
 
-  return MHD_YES;
+    return MHD_YES;
 }
 
 static enum MHD_Result
-reachard_handle_first_call (struct reachard_request *request)
-{
-  if (strcmp (request->url, "/targets/") == 0)
-    return reachard_handle_targets_first_call (request);
+reachard_handle_first_call(struct reachard_request *request) {
+    if (strcmp(request->url, "/targets/") == 0) {
+        return reachard_handle_targets_first_call(request);
+    }
 
-  return reachard_respond (request, "url not allowed", MHD_HTTP_BAD_REQUEST);
+    return reachard_respond(request, "url not allowed", MHD_HTTP_BAD_REQUEST);
 }
 
 static enum MHD_Result
-reachard_handle (void *cls, struct MHD_Connection *conn, const char *url,
-                 const char *method, const char *version,
-                 const char *upload_data, size_t *upload_data_size,
-                 void **req_cls)
-{
-  /* This function is called several times over the lifetime of a request */
-  struct reachard_request request
-      = { cls,    conn, url, method, version, upload_data, upload_data_size,
-          req_cls };
+reachard_handle(
+    void *cls, struct MHD_Connection *conn,
+    const char *url, const char *method, const char *version,
+    const char *upload_data, size_t *upload_data_size,
+    void **req_cls
+) {
+    /* This function is called several times over the lifetime of a request */
+    struct reachard_request request = {
+        cls, conn,
+        url, method, version,
+        upload_data, upload_data_size,
+        req_cls
+    };
 
-  struct reachard_connection_info *conn_info = *req_cls;
+    struct reachard_connection_info *conn_info = *req_cls;
 
-  /* First call has headers available only */
-  if (!conn_info)
-    return reachard_handle_first_call (&request);
+    /* First call has headers available only */
+    if (!conn_info) {
+        return reachard_handle_first_call(&request);
+    }
 
-  /* The following calls make body available as well */
-  return conn_info->handle (&request);
+    /* The following calls make body available as well */
+    return conn_info->handle(&request);
 }
 
 void
-reachard_complete (void *cls, struct MHD_Connection *conn, void **req_cls,
-                   enum MHD_RequestTerminationCode toe)
-{
-  reachard_connection_info_destroy (*req_cls);
+reachard_complete(
+    void *cls, struct MHD_Connection *conn,
+    void **req_cls,
+    enum MHD_RequestTerminationCode toe
+) {
+    reachard_connection_info_destroy(*req_cls);
 }
 
 static void
-reachard_interrupt (int sig, siginfo_t *info, void *ucontext)
-{
-  printf ("\rShutting down! [%d]\n", sig);
+reachard_interrupt(int sig, siginfo_t *info, void *ucontext) {
+    printf("\rShutting down! [%d]\n", sig);
 }
 
 int
-main ()
-{
-  struct MHD_Daemon *daemon
-      = MHD_start_daemon (MHD_USE_INTERNAL_POLLING_THREAD, PORT, NULL, NULL,
-                          &reachard_handle, NULL, MHD_OPTION_NOTIFY_COMPLETED,
-                          &reachard_complete, NULL, MHD_OPTION_END);
-  if (!daemon)
-    return 1;
+main() {
+    struct MHD_Daemon *daemon = MHD_start_daemon(
+        MHD_USE_INTERNAL_POLLING_THREAD, PORT,
+        NULL, NULL,
+        &reachard_handle, NULL,
+        MHD_OPTION_NOTIFY_COMPLETED, &reachard_complete, NULL,
+        MHD_OPTION_END
+    );
+    if (!daemon) {
+        return 1;
+    }
 
-  const struct sigaction act
-      = { .sa_sigaction = &reachard_interrupt, .sa_flags = SA_SIGINFO };
-  sigaction (SIGINT, &act, NULL);
-  sigaction (SIGTERM, &act, NULL);
+    const struct sigaction act = {
+        .sa_sigaction = &reachard_interrupt,
+        .sa_flags = SA_SIGINFO
+    };
+    sigaction(SIGINT, &act, NULL);
+    sigaction(SIGTERM, &act, NULL);
 
-  printf ("Listening on :%d\n", PORT);
-  pause ();
+    printf("Listening on :%d\n", PORT);
+    pause();
 
-  MHD_stop_daemon (daemon);
+    MHD_stop_daemon(daemon);
 
-  return 0;
+    return 0;
 }
