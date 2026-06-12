0bf46a15e28da0a6e7f5b0078f587a2a7292141b
Pavel Sobolev <contact@paveloom.dev>
2026-06-12 16:54:18 +0300

    Print the effective URL and time for each completed transfer

--- src/client/state.c
+++ src/client/state.c
@@ -27,6 +27,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 // See the following documentation:
 // - https://curl.se/libcurl/c/libcurl-multi.html
@@ -74,11 +75,15 @@ check_completed(CURLM *multi) {
             fprintf(stderr, "%s\n", curl_easy_strerror(err));
             fprintf(stderr, "transfer failed with error code %d\n", err);
         } else {
+            char *url;
             long status;
+            time_t epoch;
+
+            curl_easy_getinfo(easy, CURLINFO_EFFECTIVE_URL, &url);
             curl_easy_getinfo(easy, CURLINFO_RESPONSE_CODE, &status);
-            fprintf(
-                stderr, "finished the transfer with status code %ld\n", status
-            );
+            time(&epoch);
+
+            fprintf(stderr, "%s --> %ld at %jd\n", url, status, epoch);
         }
 
         curl_multi_remove_handle(multi, easy);
